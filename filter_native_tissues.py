f466d355e8a56e4be8d0a90a10130a4b1292745d
kadan02 <kada.norbert@gmail.com>
2024-12-16 16:00:24 +0100

    Cellosaurus szűrés integrálása

--- filter_native_tissues.py
+++ filter_native_tissues.py
@@ -1,7 +1,6 @@
 import pandas as pd
 import re
 
-# TODO: rendes oszlop nevek?
 
 # fájlbeolvasások
 def read_data(tsv_path):
@@ -10,7 +9,7 @@ def read_data(tsv_path):
         for line in infile:
             rows.append(line.strip().split('\t'))
     data = pd.DataFrame(rows)
-    data = data.replace('"', '', regex=True)  # lehet hogy redundáns? mindenesetre így működik.
+    data = data.replace('"', '', regex=True)
     return data
 
 
@@ -31,18 +30,25 @@ def filter_data(data, column_index, keywords):
         removed_details.append(removed_rows)
         data = data[~mask]
 
+    # Az összes eltávolított sor összevonása egy DataFrame-be
+    if removed_details:
+        removed_details = pd.concat(removed_details, ignore_index=True)
+    else:
+        removed_details = pd.DataFrame(columns=data.columns.tolist() + ['Keyword'])
+
     return data, removed_details
 
-
-tsv_path = 'tsv/hg38_TF_filtered_experiments_relevant_columns.tsv'
-data = read_data(tsv_path)
-cell_lines_main = 'cell_lines/cell_line_list_all_no_duplicates.txt'
-cell_lines_chipatlas = 'cell_lines/chip_atlas_added_cell_lines.txt'
+metadata_path = 'tsv/mm10_TF_filtered_experiments_relevant_columns.tsv'
+data = read_data(metadata_path)
+cell_lines_brenda = 'cell_lines/cell_lines_brenda.txt'
+cell_lines_mm10 = 'cell_lines/mm10_added_cell_lines.txt'
 other_keywords = 'cell_lines/keywords.txt'
+cellosaurus_mm10 = 'cell_lines/cellosaurus_mouse.tsv'
 
 # Kulcszavak
-filter_out_cell_type_class = ['Placenta', 'Gonad', 'Embryo', 'Pluripotent stem cell', 'No description']
-filter_out_cell_type = read_keywords(cell_lines_main) + read_keywords(cell_lines_chipatlas) + read_keywords(other_keywords)
+filter_out_cell_type_class = ['Placenta', 'Embryo', 'Pluripotent stem cell', 'No description', 'Embryonic fibroblast']
+filter_out_cell_type = (read_keywords(cell_lines_brenda) + read_keywords(other_keywords)
+                        + read_keywords(cellosaurus_mm10))
 filter_out_track_type = ['Epitope tags', 'GFP']
 
 data, removed_cell_type_class = filter_data(data, 2, filter_out_cell_type_class)
@@ -50,10 +56,11 @@ data, removed_cell_type = filter_data(data, 3, filter_out_cell_type)
 data, removed_track_type = filter_data(data, 1, filter_out_track_type)
 
 # Törölt adatok mentése
-removed_lines = pd.concat(removed_cell_type_class + removed_cell_type + removed_track_type, ignore_index=True)
-removed_output_file = 'tsv/hg38_removed_experiments.tsv'
+removed_lines = pd.concat([removed_cell_type_class, removed_track_type, removed_cell_type],
+                          ignore_index=True)
+removed_output_file = 'tsv/cellosaurus_test/mm10_removed_runs.tsv'
 removed_lines.to_csv(removed_output_file, index=False, sep='\t', escapechar='\\')
 
 # Megtartott adatok mentése
-native_experiments_table = 'tsv/hg38_native_experiments.tsv'
+native_experiments_table = 'tsv/cellosaurus_test/mm10_native_runs.tsv'
 data.to_csv(native_experiments_table, index=False, sep='\t', escapechar='\\')

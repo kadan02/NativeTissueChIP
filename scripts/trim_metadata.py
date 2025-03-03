8d0b27258cf1ddfdfabe00d1b591cc06917d55dc
kadan02 <kada.norbert@gmail.com>
2025-03-03 21:36:00 +0100

    Irreleváns oszlopok automatikus törlése

--- scripts/trim_metadata.py
+++ scripts/trim_metadata.py
@@ -18,9 +18,15 @@ def trim_table(input_tsv, output_tsv, variable_names):
             if not any(clean_field.startswith(var + "=") for var in variable_names):
                 new_fields.append(field)
             else:
-                print("Törölve: " + field)
+                print("Deleted: " + field)
 
         cleaned_rows.append(new_fields)
+
+    # 4. 5. és 6. oszlopok törlése (nem releváns adatok)
+    cleaned_rows = [
+        [field for i, field in enumerate(row) if i not in {4, 5, 6}]
+        for row in cleaned_rows
+    ]
 
     # A maximum nem üres stringet tartalmazó oszlopszám az összes új sor közül. A többi sor eddig lesz feltöltve üres
     # karakterekkel, hogy egységes oszlopszámú legyen
@@ -36,8 +42,9 @@ def trim_table(input_tsv, output_tsv, variable_names):
             row.extend([""] * (col_number - len(row)))
             outfile.write("\t".join(row)  + "\n")
 
+if __name__ == "__main__":
 
-fields_to_remove = ['INSDC status', 'INSDC center alias', 'INSDC center name', 'collection_date', 'biomaterial_provider',
+    fields_to_remove = ['INSDC status', 'INSDC center alias', 'INSDC center name', 'collection_date', 'biomaterial_provider',
                     'geo_loc_name', 'ENA first public', 'ENA last update', 'External Id', 'INSDC first public', 'INSDC last update',
                     'Submitter Id', 'ENA-FIRST-PUBLIC', 'ENA-LAST-UPDATE','ENA-CHECKLIST', 'antibody vendor', 'vendor',
                     'antibody manufacturer', 'chip antibody vendor','antibody manufacturer', 'antibody catalog id',
@@ -49,15 +56,15 @@ fields_to_remove = ['INSDC status', 'INSDC center alias', 'INSDC center name', '
                     'antibody brand','chip antibody manufacturer','chip ab','chip-antibody','antibody maker','chip target',
                     'sample name','ArrayExpress-Immunoprecipitate','ArrayExpress-CellType','chip antibody info','antibody vendor/provider',
                     'chip antibody catalog number','chip antibody manufacturer 1','chip antibody catalog number 1']
-"""
---- Megjegyzések: ---
-Kérdőjeles field-ek:
-donor ID - indikátora lehetnek annak, hogy nem sejtvonalas kísérlet
-source - legtöbb helyen kutatók neve, de túl tág fogalom, lehet van hasznos példa is?
-antibody type fieldek (vegülis ugyanaz mint a track type oszlop)?
-sex - valószínűleg nem releváns
-"""
+    """
+    --- Megjegyzések: ---
+    Kérdőjeles field-ek:
+    donor ID - indikátora lehetnek annak, hogy nem sejtvonalas kísérlet
+    source - legtöbb helyen kutatók neve, de túl tág fogalom, lehet van hasznos példa is?
+    antibody type fieldek (vegülis ugyanaz mint a track type oszlop)?
+    sex - valószínűleg nem releváns
+    """
 
-input_path = ''
-output_path = ''
-trim_table(input_path, output_path, fields_to_remove)
+    input_path = '../data/processed/metadata/human_cleaned.tsv'
+    output_path = '../data/processed/metadata/human_cleaned_trimmed.tsv'
+    trim_table(input_path, output_path, fields_to_remove)

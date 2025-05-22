c052ddcb6156a86dae72ee72bbdba5758a7ec60d
kadan02 <kada.norbert@gmail.com>
2025-05-22 13:36:37 +0200

    Konfigurációs sorok hozzáadása

--- scripts/filter_beds.py
+++ scripts/filter_beds.py
@@ -1,10 +1,15 @@
 import csv
 import os
 
+# Konfiguráció
+native_metadata_table = "../data/processed/classification/mistral-small31-24b_full/native_classified.tsv"
+tissue_bed_file = "../data/raw/bed/Oth.Utr.05.AllAg.AllCell.bed"
+
+
 srx_ids = {}
 # ignored_lines = set() # debug
 
-with open('../data/processed/classification/mistral-small31-24b_full/native_classified.tsv', mode='r', encoding='utf-8', ) as tsv_file:
+with open(native_metadata_table, mode='r', encoding='utf-8', ) as tsv_file:
     tsv_reader = csv.DictReader(tsv_file, delimiter='\t')
     for row in tsv_reader:
         srx_id = row['ID']
@@ -15,49 +20,45 @@ with open('../data/processed/classification/mistral-small31-24b_full/native_clas
 
 id_counter = {}
 output_files = {}
-try:
-    with open("../data/raw/bed/Oth.Utr.05.AllAg.AllCell.bed", 'r', encoding="UTF-8") as input_file:
-        next(input_file) # első sor metaadatot tartalmaz a file-ban
+with open(tissue_bed_file, 'r', encoding="UTF-8") as input_file:
+    next(input_file) # első sor metaadatot tartalmaz a file-ban
 
-        for line in input_file:
-            id_start = line.find('ID=') + 3
-            id_end = line.find(';', id_start)
-            current_srx_id = line[id_start:id_end]
+    for line in input_file:
+        id_start = line.find('ID=') + 3
+        id_end = line.find(';', id_start)
+        current_srx_id = line[id_start:id_end]
 
-            if current_srx_id in srx_ids:
-                tissue = srx_ids[current_srx_id]["Cell_type_class"]
-                tf_name = srx_ids[current_srx_id]["Transcription_Factor"]
+        if current_srx_id in srx_ids:
+            tissue = srx_ids[current_srx_id]["Cell_type_class"]
+            tf_name = srx_ids[current_srx_id]["Transcription_Factor"]
 
-                # Output path generálása
-                output_dir = "../data/processed/bed/"
-                output_filename = f'filtered_{tissue}.bed'
-                output_file_path = os.path.join(output_dir, output_filename)
-                os.makedirs(output_dir, exist_ok=True)
+            # Output path generálása
+            output_dir = "../data/processed/bed/"
+            output_filename = f'filtered_{tissue}.bed'
+            output_file_path = os.path.join(output_dir, output_filename)
+            os.makedirs(output_dir, exist_ok=True)
 
-                # Soronként egyedi ID generálása az SRA ID után (run-onként eggyel növekvő szám)
-                if current_srx_id not in id_counter:
-                    id_counter[current_srx_id] = 1
-                else:
-                    id_counter[current_srx_id] += 1
-                unique_id = f"{current_srx_id}_{id_counter[current_srx_id]:07d}"
+            # Soronként egyedi ID generálása az SRA ID után (run-onként eggyel növekvő szám)
+            if current_srx_id not in id_counter:
+                id_counter[current_srx_id] = 1
+            else:
+                id_counter[current_srx_id] += 1
+            unique_id = f"{current_srx_id}_{id_counter[current_srx_id]:07d}"
 
-                # oszlopok: chr start end unique_id track_type
-                columns = line.split('\t')
-                chr_info = f"{columns[0]}\t{columns[1]}\t{columns[2]}\t{unique_id}\t{tf_name}"
+            # oszlopok: chr start end unique_id track_type
+            columns = line.split('\t')
+            chr_info = f"{columns[0]}\t{columns[1]}\t{columns[2]}\t{unique_id}\t{tf_name}"
 
-                if output_file_path not in output_files:
-                    output_files[output_file_path] = open(output_file_path, 'a', encoding='utf-8')
-                    print(f"Opening {output_file_path} for appending...")
-                output_files[output_file_path].write(chr_info + '\n')
-            # else:
-               # ignored_lines.add(current_srx_id)
+            if output_file_path not in output_files:
+                output_files[output_file_path] = open(output_file_path, 'a', encoding='utf-8')
+                print(f"Opening {output_file_path} for appending...")
+            output_files[output_file_path].write(chr_info + '\n')
+        # else:
+            # ignored_lines.add(current_srx_id)
 
 
-    # with open('log/ignored_lines_' + tissue + ".log", 'w') as log_file:
-    #    log_file.write('\n'.join(ignored_lines))
+# with open('log/ignored_lines_' + tissue + ".log", 'w') as log_file:
+#    log_file.write('\n'.join(ignored_lines))
 
-finally:
-    print("Closing output files...")
-    for f_handle in output_files.values():
-        f_handle.close()
-    print("Finished processing.")
+for f_handle in output_files.values():
+    f_handle.close()
